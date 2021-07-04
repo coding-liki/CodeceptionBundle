e4ede423a57a9c714ba0a37c2fc29fc201d946b0
Лев Некрасов <lev.nekrasov@devim.team>
2021-07-04 16:43:07 +0300

    Обновил скрипт установки codecept, теперь он не сакивает phar если он уже есть

--- scripts/installToBin.php
+++ scripts/installToBin.php
@@ -1,15 +1,35 @@
 <?php
 declare(strict_types=1);
 
+
 checkCreateFolder(CURRENT_WORKIN_DIR.'/bin');
 
 $lastWd = getcwd();
 chdir(CURRENT_WORKIN_DIR.'/bin');
 
-exec('wget https://codeception.com/codecept.phar');
+/**
+ * @return bool
+ */
+function codeceptInstalled(): bool
+{
+    return is_file(CURRENT_WORKIN_DIR . '/bin/codecept.phar');
+}
 
-if(is_file(CURRENT_WORKIN_DIR.'/bin/codecept.phar')){
-    echo "codecept installed successfully to 'bin/codecept.phar'\n";
+function installCodecept()
+{
+    exec('wget https://codeception.com/codecept.phar');
+
+    if (codeceptInstalled()) {
+        echo "codecept успешно установлен в 'bin/codecept.phar'\n";
+    } else {
+        throw new Exception("codecept не установлен:(");
+    }
+}
+
+if(codeceptInstalled()){
+    echo "codecept уже установлен в bin/codecept.phar'\n";
+} else {
+    installCodecept();
 }
 
 chdir($lastWd);
